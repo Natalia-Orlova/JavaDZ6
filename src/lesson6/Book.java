c207aa13ddad9fd6e46e97739ac610bf4a59b4ed
NataliaOrlova <natakarelskaya@yandex.ru>
2022-12-16 12:45:29 +0400

    hw_6 solution

--- src/lesson6/Book.java
+++ src/lesson6/Book.java
@@ -9,21 +9,19 @@ public class Book {
         this.authorName = authorName;
     }
 
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getBookName () {
+        return bookName;
+    }
+
     @Override
     public String toString() {
         return "Book{" +
                 "bookName='" + bookName + '\'' +
                 ", authorName='" + authorName + '\'' +
                 '}';
-    }
-
-    public static void main(String[] args) {
-        Book book1 = new Book ("Дискретная математика", "А.А. Набебин");
-        Book book2 = new Book ("Теория алгоритмов", "В. Игошин");
-        Book book3 = new Book ("Математическая логика", "А.Н. Колмогоров");
-        Book book4 = new Book ("История географии", "Е.И. Перцик");
-
-        System.out.println(book1);
-
     }
 }
--- src/lesson6/Reader.java
+++ src/lesson6/Reader.java
@@ -1,24 +1,5 @@
 package lesson6;
 
-//Определить класс Reader, хранящий такую информацию о пользователе библиотеки:
-//
-//1. ФИО,
-//2. номер читательского билета,
-//3. факультет,
-//4. дата рождения,
-//5. телефон.
-//6. Методы (экземпляра, без слова static)  takeBook(), returnBook().
-//7. Разработать программу, в которой создается массив объектов данного класса.
-//8. Перегрузить методы takeBook(), returnBook():
-//- takeBook, который будет принимать количество взятых книг. Выводит на консоль сообщение "Петров В. В. взял 3 книги".
-//- takeBook, который будет принимать переменное количество названий книг. Выводит на консоль сообщение "Петров В. В.
-//      взял книги: Приключения, Словарь, Энциклопедия".
-//- takeBook, который будет принимать переменное количество (Тип... parameterName) объектов класса Book
-//      (создать новый класс, содержащий имя и автора книги). Выводит на консоль сообщение "Петров В. В. взял книги:
-//      Приключения, Словарь, Энциклопедия".
-//9. Аналогичным образом перегрузить метод returnBook(). Выводит на консоль сообщение "Петров В. В. вернул книги:
-//      Приключения, Словарь, Энциклопедия". Или  "Петров В. В. вернул 3 книги".
-
 public class Reader {
     String name;
     Integer libraryCard;
@@ -51,31 +32,53 @@ public class Reader {
 
     }
 
+    //takeBook, который будет принимать количество взятых книг.
+    // Выводит на консоль сообщение "Петров В. В. взял 3 книги"
     public void takeBook(int books) {
-        System.out.println(this.name + " взял " + books + " книг");
+        System.out.println(this.name + " взял(а) " + books + " книг(и)");
     }
+
+    //takeBook, который будет принимать переменное количество названий книг.
+    // Выводит на консоль сообщение "Петров В. В.
+    // взял книги: Приключения, Словарь, Энциклопедия".
+    public void takeBook(String... books) {
+        System.out.print(this.name + " взял(а) книги: ");
+        for (int i = 0; i < books.length - 1; i++) {
+            System.out.print(books[i] + ", ");
+        }
+        System.out.println(books[books.length - 1]);
+    }
+
+    public void takeBook(Book... booksList) {
+        System.out.print(this.name + " взял(а) книги: ");
+        for (int i = 0; i < booksList.length - 1; i++) {
+            System.out.print(booksList[i].bookName + ", ");
+        }
+        System.out.println(booksList[booksList.length - 1].bookName);
+    }
+
 
     public void returnBook() {
 
     }
 
     public void returnBook(int books){
-        System.out.println(this.name + " вернул " + books + " книг");
+        System.out.println(this.name + " вернул(а) " + books + " книг(и)");
     }
 
-    public static void main(String[] args) {
-        Reader reader1 = new Reader("Иванов И.И.", 123,
-                "ФМФИ", "12.12.2002", "9252754623");
-        Reader reader2 = new Reader ("Соколов П.П.", 245,
-                "ЕГФ", "23.10.2004", "123456789");
-        System.out.println(reader1);
-        System.out.println(reader2);
-
-        reader1.takeBook(10);
-        reader1.returnBook(5);
-
-        reader2.takeBook(4);
-        reader2.returnBook(3);
+    public void returnBook(String... books) {
+        System.out.print(this.name + " вернул(а) книги: ");
+        for (int i = 0; i < books.length - 1; i++) {
+            System.out.print(books[i] + ", ");
+        }
+        System.out.println(books[books.length - 1]);
     }
 
+    public void returnBook(Book... booksList) {
+        System.out.print(this.name + " вернул(а) книги: ");
+        for (int i = 0; i < booksList.length - 1; i++) {
+            System.out.print(booksList[i].bookName + ", ");
+        }
+        System.out.println(booksList[booksList.length - 1].bookName);
+    }
 }
